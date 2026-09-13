cb6f9c0e3af1d8a79c05e8088d829549978bdb83
Diyarqilich <diyarbekpayzillaev@gmail.com>
2026-09-13 17:12:54 +0500

    Use DATABASE_URL for production database

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -122,6 +122,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
 ]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://s1ndze-shoop.onrender.com",
+]
+
 CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
